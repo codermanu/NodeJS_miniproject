0ee6078bf3e1ee764ce4725ecbb9052af181fefc
codermanu <manu.nevish@gmail.com>
2019-05-28 22:53:43 +0530

    Configure MongoDB and JWT

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Manu:nadeesha@cluster0-krfxh.mongodb.net/test?retryWrites=true",
+    secreteOrKey:"secret"
+};
+
+module.exports = {
+    mongoURI: "mongodb+srv://Manu:nadeesha@cluster0-krfxh.mongodb.net/test?retryWrites=true",    
+    secretOrKey: 'yoursecretkeyishere'
+};
+
